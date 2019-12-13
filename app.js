0b77cf3a69c1931cbce9e3477c09193ddbae59ab
naterush <naterush1997@gmail.com>
2019-12-12 22:29:06 -0500

    add website

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Happy secret santa, you rat'))
+// set up rendering
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// set up public shit
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.render('main.html');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
